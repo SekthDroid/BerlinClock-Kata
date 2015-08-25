2c3efb8627f8d9deab8beb3a1223606bdb3d7c3c
SekthDroid <secthndamon@gmail.com>
2015-08-25 23:20:12 +0200

    Added five munges row implementation

--- berlinclock/BerlinClockConverter.py
+++ berlinclock/BerlinClockConverter.py
@@ -15,3 +15,21 @@ class BerlinClockConverter(object):
             return result
         result = result.ljust(4, "O")
         return result
+
+    def get_five_minutes(self, param):
+        minutes = int(param.split(":")[1])
+        if minutes is 0:
+            return "OOOOOOOOOOO"
+
+        result = ""
+        for i in range(5, 60, 5):
+            if i <= minutes:
+                result += "Y"
+            else:
+                result += "O"
+
+        result = list(result)
+        for i in range(2, len(result), 3):
+            if result[i] is not "O":
+                result[i] = "R"
+        return "".join(result)
--- test/BerlinClockTest.py
+++ test/BerlinClockTest.py
@@ -5,7 +5,6 @@ __author__ = 'SekthDroid'
 
 
 class BerlinClockConverterTest(TestCase):
-
     def test_should_return_single_minutes_row(self):
         berlin_clock = BerlinClockConverter()
         self.assertEqual("OOOO", berlin_clock.get_single_minutes("00:00:00"))
@@ -13,3 +12,11 @@ class BerlinClockConverterTest(TestCase):
         self.assertEqual("YYOO", berlin_clock.get_single_minutes("12:32:00"))
         self.assertEqual("YYYY", berlin_clock.get_single_minutes("12:34:00"))
         self.assertEqual("OOOO", berlin_clock.get_single_minutes("12:35:00"))
+
+    def test_should_return_five_minutes_row(self):
+        berlin_clock = BerlinClockConverter()
+        self.assertEqual("OOOOOOOOOOO", berlin_clock.get_five_minutes("00:00:00"))
+        self.assertEqual("YYRYYRYYRYY", berlin_clock.get_five_minutes("23:59:59"))
+        self.assertEqual("OOOOOOOOOOO", berlin_clock.get_five_minutes("12:04:00"))
+        self.assertEqual("YYRYOOOOOOO", berlin_clock.get_five_minutes("12:23:00"))
+        self.assertEqual("YYRYYRYOOOO", berlin_clock.get_five_minutes("12:35:00"))

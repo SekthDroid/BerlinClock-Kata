1a08cd5251d486f149c3cb1d57972bab108f7d10
SekthDroid <secthndamon@gmail.com>
2015-08-25 23:23:44 +0200

    Refactored single minutes

--- berlinclock/BerlinClockConverter.py
+++ berlinclock/BerlinClockConverter.py
@@ -1,26 +1,18 @@
+OFF = "O"
+YELLOW = "Y"
 __author__ = 'SekthDroid'
 
 
 class BerlinClockConverter(object):
     def get_single_minutes(self, param):
         minutes = int(param.split(":")[1][1])
-        if minutes is 0:
-            return "OOOO"
 
-        if minutes > 4:
-            minutes -= 5
-
-        result = "".rjust(minutes, "Y")
-        if len(result) is 4:
-            return result
-        result = result.ljust(4, "O")
-        return result
+        minutes = minutes - 5 if minutes > 4 else minutes
+        result = "".rjust(minutes, "%s" % YELLOW)
+        return result.ljust(4, "%s" % OFF) if len(result) < 4 else result
 
     def get_five_minutes(self, param):
         minutes = int(param.split(":")[1])
-        if minutes is 0:
-            return "OOOOOOOOOOO"
-
         result = ""
         for i in range(5, 60, 5):
             if i <= minutes:

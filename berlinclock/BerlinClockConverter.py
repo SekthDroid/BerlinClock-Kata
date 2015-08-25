5f4303330ff15892c47468f2db30e487d35b4c1e
SekthDroid <secthndamon@gmail.com>
2015-08-25 22:51:10 +0200

    Added test and implementation for the single minutes row

--- berlinclock/BerlinClockConverter.py
+++ berlinclock/BerlinClockConverter.py
@@ -2,4 +2,16 @@ __author__ = 'SekthDroid'
 
 
 class BerlinClockConverter(object):
-    pass
+    def get_single_minutes(self, param):
+        minutes = int(param.split(":")[1][1])
+        if minutes is 0:
+            return "OOOO"
+
+        if minutes > 4:
+            minutes -= 5
+
+        result = "".rjust(minutes, "Y")
+        if len(result) is 4:
+            return result
+        result = result.ljust(4, "O")
+        return result
--- test/BerlinClockTest.py
+++ test/BerlinClockTest.py
@@ -1,7 +1,15 @@
 from unittest import TestCase
+from berlinclock.BerlinClockConverter import BerlinClockConverter
 
 __author__ = 'SekthDroid'
 
 
 class BerlinClockConverterTest(TestCase):
-    pass
+
+    def test_should_return_single_minutes_row(self):
+        berlin_clock = BerlinClockConverter()
+        self.assertEqual("OOOO", berlin_clock.get_single_minutes("00:00:00"))
+        self.assertEqual("YYYY", berlin_clock.get_single_minutes("23:59:59"))
+        self.assertEqual("YYOO", berlin_clock.get_single_minutes("12:32:00"))
+        self.assertEqual("YYYY", berlin_clock.get_single_minutes("12:34:00"))
+        self.assertEqual("OOOO", berlin_clock.get_single_minutes("12:35:00"))
